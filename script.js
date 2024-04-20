e29b195787788568a0602247c00ffb514d964e2b
Guilherme Camargo <guilherme.albani2004@gmail.com>
2024-04-19 22:01:31 -0300

    Changing and updating the style of the sidebar and how it works. Adding meta tags

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ import { confetti } from "@tsparticles/confetti";
 import { tsParticles } from "@tsparticles/engine";
 
 const buttonCallToAction = document.querySelector(".introduction__button-call-to-action")
-const buttonOpenSidebar = document.querySelector(".header__menu-open-sidebar-button")
-const buttonCloseSidebar = document.querySelector(".header__menu-close-sidebar-button")
+const buttonSidebar = document.querySelector(".header__menu-sidebar-button")
 const sidebar = document.querySelector(".header__menu--sidebar")
+const allIconBars = document.querySelectorAll(".header__menu-icon-bar")
+const overlay = document.querySelector(".overlay")
+
+let isSidebarOpen = false
 
 async function loadParticles() {
     await loadAll(tsParticles)
@@ -18,6 +21,32 @@ async function loadParticles() {
 
 loadParticles()
 
+function openSidebar() {
+    overlay.style.display = "block"
+    sidebar.classList.add("header__menu--sidebar-animation")
+    allIconBars[0].classList.add("header__menu-icon-bar--animation-first")
+    allIconBars[1].classList.add("header__menu-icon-bar--animation-second")
+    allIconBars[2].classList.add("header__menu-icon-bar--animation-third")
+}
+
+function closeSidebar() {
+    overlay.style.display = "none"
+    sidebar.classList.remove("header__menu--sidebar-animation")
+    allIconBars[0].classList.remove("header__menu-icon-bar--animation-first")
+    allIconBars[1].classList.remove("header__menu-icon-bar--animation-second")
+    allIconBars[2].classList.remove("header__menu-icon-bar--animation-third")
+}
+
+function sidebarAction(isOpen) {
+    isSidebarOpen = !isOpen
+
+    if(isSidebarOpen) {
+        openSidebar()
+    } else {
+        closeSidebar()
+    }
+}
+
 buttonCallToAction.addEventListener('click', () => {
     confetti({
         particleCount: 100,
@@ -26,10 +55,13 @@ buttonCallToAction.addEventListener('click', () => {
     });
 })
 
-buttonOpenSidebar.addEventListener('click', () => {
-    sidebar.style.display = "flex"
+buttonSidebar.addEventListener('click', () => {
+    sidebarAction(isSidebarOpen)
 })
 
-buttonCloseSidebar.addEventListener('click', () => {
-    sidebar.style.display = "none"
+overlay.addEventListener('click', () => {
+    if(isSidebarOpen) {
+        isSidebarOpen = !isSidebarOpen
+        closeSidebar()
+    }
 })
