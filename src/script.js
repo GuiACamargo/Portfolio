6894180f4c11ce709f3adb1f5c44e0c4b61cacc3
Guilherme Camargo <guilherme.albani2004@gmail.com>
2024-05-06 21:39:07 -0300

    Changing styles and adding some logic to see if the device is mobile or desktop

--- src/script.js
+++ src/script.js
@@ -7,14 +7,82 @@ import { loadParticlesRepulseInteraction } from '@tsparticles/interaction-partic
 import { loadParallaxMover } from '@tsparticles/move-parallax';
 import { loadEasingCubicPlugin } from '@tsparticles/plugin-easing-cubic';
 
+const introduction = document.querySelector('.introduction');
 const buttonSidebar = document.querySelector('.header__menu-sidebar-button');
 const sidebar = document.querySelector('.header__menu--sidebar');
 const allIconBars = document.querySelectorAll('.header__menu-icon-bar');
 const overlay = document.querySelector('.overlay');
 const allSidebarAnchorButton = document.querySelectorAll('.header__menu-anchor--sidebar');
 const toast = document.querySelector('.toast');
+const tsparticlesDiv = document.querySelector('#tsparticles');
 
 let isSidebarOpen = false;
+
+function isTouchDevice() {
+    const screenSize = 480;
+    const isTouch = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+    const isSmallScreen = window.innerWidth < screenSize || screen.width < screenSize;
+
+    console.log(isTouch);
+    console.log(isSmallScreen);
+
+    return isTouch && isSmallScreen;
+}
+
+async function loadParticles() {
+    loadBasic(tsParticles);
+    loadEasingCubicPlugin(tsParticles);
+    loadExternalRepulseInteraction(tsParticles);
+    loadParticlesRepulseInteraction(tsParticles);
+    loadParticlesLinksInteraction(tsParticles);
+    loadParallaxMover(tsParticles);
+    await launchConfetti(0, 0, 0, 0);
+    await tsParticles
+        .load({
+            id: 'tsparticles',
+            url: '/presets/default.json',
+        })
+        .then(() => {
+            toast.classList.remove('toast--animation');
+        })
+        .catch((error) => console.log(error));
+}
+
+function setupPage() {
+    if(!isTouchDevice()) {
+        tsparticlesDiv.classList.remove("tsparticles-mobile");
+        introduction.classList.remove("introduction-mobile")
+        toast.classList.add('toast--animation')
+        loadParticles();
+    } else {
+        launchConfetti(0, 0, 0, 0)
+        tsparticlesDiv.classList.add("tsparticles-mobile");
+        introduction.classList.add("introduction-mobile")
+    }
+}
+
+setupPage();
+
+let wasTouchDevice = isTouchDevice();
+
+function setupPageResizeListener() {
+    const isNowTouchDevice = isTouchDevice();
+    if(wasTouchDevice !== isNowTouchDevice) {
+        wasTouchDevice = isNowTouchDevice;
+        if(!isNowTouchDevice) {
+            tsparticlesDiv.classList.remove("tsparticles-mobile");
+            introduction.classList.remove("introduction-mobile")
+            toast.classList.add('toast--animation')
+            loadParticles();
+        } else {
+            launchConfetti(0, 0, 0, 0)
+            tsparticlesDiv.classList.add("tsparticles-mobile");
+            introduction.classList.add("introduction-mobile")
+        }
+    }
+}
+
+window.addEventListener('resize', setupPageResizeListener);
 
 function openSidebar() {
     overlay.style.display = 'block';
@@ -53,15 +121,6 @@ async function launchConfetti(count, spread, positionX, positionY) {
     });
 }
 
-function launchConfettiFromButton(eventX, eventY) {
-    const widthView = window.innerWidth;
-    const heightView = window.innerHeight;
-    const xValue = (eventX / widthView) * 100;
-    const yValue = (eventY / heightView) * 100;
-
-    launchConfetti(60, 60, xValue, yValue);
-}
-
 allSidebarAnchorButton.forEach((anchor) => {
     anchor.addEventListener('click', () => {
         if (isSidebarOpen) {
@@ -81,24 +140,3 @@ overlay.addEventListener('click', () => {
         closeSidebar();
     }
 });
-
-async function loadParticles() {
-    loadBasic(tsParticles);
-    loadEasingCubicPlugin(tsParticles);
-    loadExternalRepulseInteraction(tsParticles);
-    loadParticlesRepulseInteraction(tsParticles);
-    loadParticlesLinksInteraction(tsParticles);
-    loadParallaxMover(tsParticles);
-    await launchConfetti(0, 0, 0, 0);
-    await tsParticles
-        .load({
-            id: 'tsparticles',
-            url: '/presets/default.json',
-        })
-        .then(() => {
-            toast.classList.remove('toast--animation');
-        })
-        .catch((error) => console.log(error));
-}
-
-loadParticles();
